7c73d554a466f94c10ed6e9785fec452ab31b5db
zhizhizhiwang <1073171253@qq.com>
2024-11-09 11:53:49 +0800

    重构版式

---
+++ buttom.js
@@ -0,0 +1,21 @@
+const toggleButton_1 = document.getElementById('toggle-button-1');
+const element_1 = document.getElementById('element-1');
+
+const toggleButton_2 = document.getElementById('toggle-button-2');
+const element_2 = document.getElementById('element-2');
+
+toggleButton_1.addEventListener('click', function () {
+    if (element_1.style.height === '0px') {
+        element_1.style.height = '1000px';
+    } else {
+        element_1.style.height = '0px';
+    }
+});
+
+toggleButton_2.addEventListener('click', function () {
+    if (element_2.style.height === '0px') {
+        element_2.style.height = '1000px';
+    } else {
+        element_2.style.height = '0px';
+    }
+});
