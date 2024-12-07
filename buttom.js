887e76498feb603990ebbaa66aff3816b220cdbd
zhizhizhiwang <1073171253@qq.com>
2024-12-07 19:26:03 +0800

    增加了侧边栏

--- buttom.js
+++ buttom.js
@@ -6,7 +6,7 @@ const element_2 = document.getElementById('element-2');
 
 toggleButton_1.addEventListener('click', function () {
     if (element_1.style.height === '0px') {
-        element_1.style.height = '1000px';
+        element_1.style.height = 'auto';
     } else {
         element_1.style.height = '0px';
     }
@@ -14,7 +14,7 @@ toggleButton_1.addEventListener('click', function () {
 
 toggleButton_2.addEventListener('click', function () {
     if (element_2.style.height === '0px') {
-        element_2.style.height = '700px';
+        element_2.style.height = 'auto';
     } else {
         element_2.style.height = '0px';
     }
